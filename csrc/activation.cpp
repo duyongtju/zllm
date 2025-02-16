1980f86aa59df5f6e2853446a7d22772d1b4bb74
duyong02 <duyong02@baidu.com>
2025-02-16 13:20:31 +0800

    新增 kernel activation; 新增构建打包脚本

---
+++ csrc/activation.cpp
@@ -0,0 +1,29 @@
+#include <torch/extension.h>
+
+void silu_and_mul(
+    torch::Tensor &out,
+    torch::Tensor &input);
+
+void gelu_new(
+    torch::Tensor &out,
+    torch::Tensor &input);
+
+void gelu_fast(
+    torch::Tensor &out,
+    torch::Tensor &input);
+
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m)
+{
+    m.def(
+        "silu_and_mul",
+        &silu_and_mul,
+        "Activation function used in SwiGLU.");
+    m.def(
+        "gelu_new",
+        &gelu_new,
+        "GLUE implementation used in GPT-2.");
+    m.def(
+        "gelu_fast",
+        &gelu_fast,
+        "Approximated GLUE implementation.");
+}
